1bf4978758dbcaf89218c0f788ab686ee17d684b
Amal <apptalid@gmail.com>
2022-05-29 23:47:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// function status(e) {
+//   e.innerText = "Logout";
+// }
+function status(e) {
+  e.innerText = "Logout";
+}
+
+function removed(e) {
+  e.remove();
+}
+function likes() {
+  alert("This page says\nninja was liked");
+}
